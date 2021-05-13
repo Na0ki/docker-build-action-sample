139e691b49a58c6cab41d444b60d19aa8ed4874f
ahiru <wellingtonnmaeda0524@gmail.com>
2021-05-13 11:02:07 +0900

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:14-buster-slim
+
+WORKDIR /usr/bin/src
+
+COPY package*.json .
+
+RUN npm ci --only=production
+
+COPY . .
+
+CMD [ "node", "server.js" ]
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => {
+    res.json({message: 'hello world!'});
+});
+
+app.use((_err, _req, res, _next) => {
+    res.status(500).send('some error occurred');
+});
+
+const server = app.listen(port, () => {
+    console.log(`app is running at ${port}`);
+});
+
+const shutdown = () => {
+    console.log('shutting down gracefully...');
+    server.close(() => {
+        console.log('app has been terminated');
+        process.exit(0);
+    });
+};
+
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
